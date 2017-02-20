fa299d0d6d14390042626b3c36efdf6009edefda
Anderson R soares <anderson.r.soares@gmail.com>
2017-02-20 17:14:49 -0300

    teste componentes xamarim forms

---
+++ TesteFormModulo4/TesteFormModulo4/TesteFormModulo4Page.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace TesteFormModulo4
+{
+	public partial class TesteFormModulo4Page : ContentPage
+	{
+		public TesteFormModulo4Page()
+		{
+			InitializeComponent();
+		}
+	}
+}
