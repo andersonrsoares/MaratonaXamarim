507e0eb7e08e10eb36d3396962f8a46f5d882f0d
Anderson R soares <anderson.r.soares@gmail.com>
2017-02-22 14:10:14 -0300

    value converter

---
+++ FormAssistControl/FormAssistControl/View/ValueConverter/AverageToColorConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace FormAssistControl
+{
+	public class AverageToColorConverter:IValueConverter
+	{
+		public AverageToColorConverter()
+		{
+		}
+
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			double avarage = (double)value;
+			return avarage < 60 ? Color.Red : Color.Blue;
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
